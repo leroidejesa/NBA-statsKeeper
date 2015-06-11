465c78fcf4182289daf98c6bd4ad322d12605e7e
leroi & brent <student@epicodus.com>
2015-06-11 16:32:53 -0700

    add columns, fix a few more bugs

--- router.js
+++ router.js
@@ -2,8 +2,11 @@ Statskeeper.Router.map(function() {
   this.resource('teams', {path: '/'}, function() {
     this.resource('team', {path:'teams/:team_id'}, function() {
       this.resource('new-player');
+      this.resource('player', {path:'/:player_id'}, function() {
+        this.route('player');
       });
-    this.resource('player', {path:'/:player_id'});
+
+      });
   });
 
   });
